f308f3d714b2057214a28cb1962c4a37b24484a7
ihtyc <ihtyc@users.noreply.github.com>
2017-07-25 19:30:24 +0800

    comment

--- zhihu.py
+++ zhihu.py
@@ -1,42 +1,34 @@
-#coding=utf8
 '''
 Created on Jul 15 2017
 author: ihtyc
-description: parse json of zhihu question-37787176 to get image url, and download
 '''
 import urllib.request
-import urllib.parse
-import lxml.etree
-import http.cookiejar
 import gzip
 import re
-import os
+import sys
 import time
 import random
 import json
-import sys
 
-#设置存储目录
-workSpace = os.path.join(os.path.expanduser('~'), 'desktop', 'zhihu')
+#defult directory:'~/desktop/zhihuCrawl'
+workSpace = os.path.join(os.path.expanduser('~'), 'desktop', 'zhihuCrawl')
 if not os.path.exists(workSpace):
     os.makedirs(workSpace)
-#以相对路径存储图片
+#save by the relative path
 os.chdir(workSpace)
 
 def getHeaders():
     headers = {}
-    #将请求头放在文件headers中
+    #the headers info in file 'headers'
     if not os.path.exists('headers'):
         sys.exit('no headers in %s...' % os.getcwd())
 
     with open('headers', 'r', encoding='utf8') as fi:
         for line in fi:
-            #去'\n'
             line = line.rstrip()
-            #空行
             if not line:
                 continue
-            #以第一个冒号为断点读取数据
+            #separate by the first colon
             index = line.find(':')
             headers[line[:index]] = line[index+1:]
     return headers
@@ -44,50 +36,45 @@ def getHeaders():
 headers = getHeaders()
 
 def getJson(offset=-1, headers=headers):
-    #自定义当offset为-1时访问offset=0的json，仅用于getTotals，不打印输出信息
+    #no print info
     if offset == -1:
         offset = 0
     else:
         print('--- offset: %s...' % offset)
-    
-    #如果URL请求方法为POST，一般会附加数据，并且对数据进行编码
-    #data = {}
-    #data = urllib.parse.urlencode(data)
 
     url = 'https://www.zhihu.com/api/v4/questions/37787176/answers?include=data%5B*%5D.is_normal%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=' + str(offset) + '&limit=20&sort_by=default'
 
     request = urllib.request.Request(url, data=None, headers=headers)
     response = urllib.request.urlopen(request)
-    #从Request headers中的Accept-Encoding:gzip, deflate, br可以发现请求的数据经过gzip压缩
     jsonResult = gzip.decompress(response.read())  
     
-    #返回浮点随机数
+    #random float
     time.sleep(random.uniform(1, 2))
     return jsonResult
 
 def parseJson(jsonResult):
     print('parse json...')
-    #response返回数据为字节型数据，需要进行解码为str类型的数据
+    #bytes to str
     js = json.loads(jsonResult.decode('utf-8'))
 
     imgUrlSet = set()
     invalidImgUrlSet = set()
-    #一个Json中有20个用户的数据，从每个用户中获取其中图片的URL
     skipCount = 0
     for dataDict in js['data']:
-        #以点赞数过滤
+        #filter by voteup
         if dataDict['voteup_count'] < 100:
             print('\rskip %s user...' % skipCount, end='')
             continue
-        #在json['data']中，图片是以标签的格式嵌入的，先提取标签
+        #get the label in js['data']
         result = re.findall('<img .*?>', dataDict['content'])
         for res in result:
-            #再从标签中提取URL
+            #get the image url from label
             imgRes = re.search('src="(.*?)"', res)
             if imgRes:
                 orgImgUrl = imgRes.groups()[0]
-                #判断URL是否为以图片格式为后缀
+                #if the suffix
                 if re.search('_.*?(jpg|png|gif|jpeg)$',orgImgUrl):
+                    #remove '_.' in the url
                     imgUrl = re.sub('_.*?(jpg|png|gif|jpeg)$', r'.\1', orgImgUrl)
                     imgUrlSet.add(imgUrl)
                 else:
@@ -99,7 +86,7 @@ def parseJson(jsonResult):
     return imgUrlSet
 
 def getTotals():
-    #从json中直接获得总回答数目
+    #total answers
     print('--- totals: ', end='')
     js = json.loads(getJson().decode('utf-8'))
     totals = js['paging']['totals']
@@ -114,7 +101,7 @@ def saveImg(imgUrl, imgName):
     try:
         response = urllib.request.urlopen(request)
         res = response.read()
-        #返回的数据为字节数据，直接以2进制写入即可
+        #save image
         with open(imgName, 'wb') as fi:
             fi.write(res)
     except:
@@ -133,6 +120,7 @@ def download(imgUrlSet):
     for imgUrl in imgUrlSet:
         count += 1
         imgName = imgUrl.split('/')[-1]
+        #if not downloaded yet
         if imgName not in imgSet:
             new += 1
             imgSet.add(imgName)
